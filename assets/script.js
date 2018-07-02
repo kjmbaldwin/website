17fc2778db40c2cfc1bce532339d1684a0950d3a
Kyle Baldwin <kylejmbaldwin@gmail.com>
2018-07-01 22:33:54 -0400

    inital files

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+
+//keeps track of where the banner is
+function sticky_relocate() {
+  var window_top = $(window).scrollTop();
+  var div_top = $('#content-anchor').offset().top;
+  if (window_top > div_top) {
+      $('#sticky').addClass('stuck');
+      $('#sticky-phantom').show();
+  } else {
+      $('#sticky').removeClass('stuck');
+      $('#sticky-phantom').hide();
+  }
+}
+
+$(function () {  
+  $(window).scroll(sticky_relocate);  
+  sticky_relocate();  
+});
+
+$(".social-icon").mouseover(function () {
+  $(this).attr('src', $(this).data("hover"));
+}).mouseout(function () {
+  $(this).attr('src', $(this).data("src"));
+});
